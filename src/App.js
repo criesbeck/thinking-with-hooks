b8bc253c9b7b6f1748b08d1ece59d239f3503778
Chris Riesbeck <chris.riesbeck@gmail.com>
2019-03-14 16:33:50 -0500

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,28 +1,61 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
+import React, {useState} from 'react';
 import './App.css';
 
-class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
-    );
-  }
+function ProductList(props) {
+  const {products} = props;
+  const items = products.map(product =>
+    <li key={product.name}>{product.name} {product.price}</li>
+  );
+
+  return <ul className="product-list">{items}</ul>
+}
+
+function ProductGroup(props) {
+  const {group, filterFn} = props;
+  const {tag, products} = group;
+  const filtered = products.filter(filterFn);
+  return filtered.length === 0 ? null : (
+    <div>{tag}
+      <ProductList products={filtered} />
+    </div>
+  );
+}
+
+function ProductGroupList(props) {
+  const {groups, filterFn} = props;
+  return groups.map(group => 
+    <ProductGroup group={group} key={group.tag} filterFn={filterFn} />);
+}
+
+function ControlBar(props) {
+  const {inStockOnly, setInStockOnly, filterText, setFilterText} = props;
+  const toggleInStockOnly = () => setInStockOnly(!inStockOnly);
+  const handleFilterChange = (evt) => setFilterText(evt.target.value);
+  return (
+    <div>
+      <label>
+        <input type="checkbox" checked={inStockOnly} onChange={toggleInStockOnly} /> 
+        In stock only
+      </label>;
+      <input type="text" placeholder="Filter..." value={filterText} onChange={handleFilterChange} />
+    </div>
+  )
+}
+
+function App(props) {
+  const {groups} = props;
+  const [inStockOnly, setInStockOnly] = useState(false);
+  const [filterText, setFilterText] = useState('');
+  const re = new RegExp(filterText, "i");
+  const filterFn = product => 
+    (!inStockOnly || product.stocked) && product.name.search(re) !== -1;
+  return (
+    <React.Fragment>
+      <ControlBar inStockOnly={inStockOnly} setInStockOnly={setInStockOnly}
+        filterText={filterText} setFilterText={setFilterText} />
+      <ProductGroupList groups={groups} filterFn={filterFn} />
+    </React.Fragment>
+  );
 }
 
 export default App;
