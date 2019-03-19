97e8758c2a79f3b7faa7b901a93d2fc84fbffe25
Chris Riesbeck <chris.riesbeck@gmail.com>
2019-03-19 15:27:51 -0500

    fixed App.test warning with react-testing-library

--- src/App.test.js
+++ src/App.test.js
@@ -1,9 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import {act} from 'react-testing-library';
 import App from './App';
 
 it('renders without crashing', () => {
   const div = document.createElement('div');
-  ReactDOM.render(<App />, div);
-  ReactDOM.unmountComponentAtNode(div);
+  act(() => {
+    ReactDOM.render(<App />, div);
+  });
+  act(() => {
+    ReactDOM.unmountComponentAtNode(div);
+  });
 });
